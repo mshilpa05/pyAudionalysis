4d21b887b2267ea72b0e4418dbd02f836356b89b
shilpa mathew <mshilpa0596@gmail.com>
2019-09-02 16:32:32 +1200

    5 classifers added PyAudio Analysis has 5 classifiers: 1) SVM 2) KNN 3) random forest 4) gradient boosting 5) extra trees

--- audioClassification/createClassifierModel.py
+++ audioClassification/createClassifierModel.py
@@ -11,5 +11,29 @@ print (subdirectories)
 
 subdirectories = [dirname + "/" + subDirName for subDirName in subdirectories]
 print(subdirectories)
+#pyAudioAnalysis has 5 classifiers:
+#   1)SVM
+#   2)KNN
+#   3)randomForest
+#   4)GradientBoosting
+#   5)Extra Trees
 
+#SVM classifier used:
 aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmModel", False)
+
+#KNN classifier used
+aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn", "knnModel", False)
+
+#Extra Trees Model used
+aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "extratrees", "extratreesModel", False)
+
+#Gradient Boosting classifier used
+aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "gradientboosting", "gradientboostingModel", False)
+
+#RandomForest classifier used
+aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "randomforest", "randomforestModel", False)
+
+
+
+
+
